845cb45f93e47fc502516312f53a7f5bacfab1f5
serverbread <qjy20080319@yeah.net>
2023-10-01 17:26:02 +0800

    添加了用户登录功能

--- routers/user.js
+++ routers/user.js
@@ -6,9 +6,11 @@ const fs = require('fs');
 const SBLog = require('../SBLog.js')
 const jwt = require('jsonwebtoken')
 const YAML = require('yaml');
+const sqlite3 = require('sqlite3');
 
 const config = YAML.parse(fs.readFileSync('config.yml', 'utf-8')); // 配置文件
 const logger = new SBLog('debug');
+const db = new sqlite3.Database(config.database.sqlite.path, err => logger.error(err));
 
 // client.on('error', err => logger.error(err))
 
@@ -25,6 +27,43 @@ router.all('/login', (req, res) => {
         res.setHeader('Content-Type', 'application/json; charset=utf-8');
         
         const { username, password } = req.body;
+
+        // 使用数据库获取数据
+        db.get(`SELECT * FROM USERDATA WHERE id == '${username}'`, (err, row) => {
+            // 检测用户名是否正确
+            if (!row) {
+                logger.error('查无此人');
+                // logger.debug(`SELECT * FROM USERDATA WHERE id == '${username}'`);
+                data.error = true;
+                data.message = '登录失败！请检查用户名与密码是否正确！'
+                res.end(JSON.stringify(data));
+                return;
+            }
+            logger.info(`成功从数据库中找到了${username}！`);
+            // 检测密码是否正确
+            if (row.password != password) {
+                data.error = true;
+                data.message = '登录失败！请检查用户名与密码是否正确！'
+                res.end(JSON.stringify(data));
+                logger.error(`${username}的密码错误！`);
+                return;
+            }
+            logger.info(`${username}的密码正确！`);
+            // 使用jwt签名
+            jwt.sign(
+                { username },
+                config.server.jwtKey,
+                { expiresIn: '10m' },
+                (err, token) => {
+                    data.message = '登录成功'
+                    logger.info(`JWT签名成功！${token}`);
+                    data.token = token;
+                    res.setHeader('Set-Cookie', `sb_web-token=${token}`);  //设置cookie
+                    res.end(JSON.stringify(data));
+                }
+            );
+        })
+        /*
         if (username === 'xiaym' && password === '114514') {
             jwt.sign(
                 { username },
@@ -43,6 +82,7 @@ router.all('/login', (req, res) => {
             res.end(JSON.stringify(data));
         }
         //logger.debug(JSON.stringify(req.body));
+        */
 
     } else {
         data.error = true;
